2452b2bc2b2e42228d3b745e52fbf3a3425fea87
MiggeJ71 <jakobsson.mikael@gmail.com>
2022-02-06 20:54:36 +0100

    Changes to be committed: 	new file:   esp32_battery_bme280_mqtt/MQTT_access.h 	new file:   esp32_battery_bme280_mqtt/esp32_battery_bme280_mqtt.ino 	new file:   esp32_battery_bme280_mqtt/log.h 	new file:   esp32_battery_bme280_mqtt/wifi_access.h

---
+++ esp32_battery_bme280_mqtt/MQTT_access.h
@@ -0,0 +1,5 @@
+// MQTT cridentials to connect to my IoT network
+#define MQTT_USER "mqttuser"
+#define MQTT_PASSWORD "ryZ191wjE569!"
+#define MQTT_BROKER "192.168.1.205"
+#define MQTT_PORT 1883
---
+++ esp32_battery_bme280_mqtt/wifi_access.h
@@ -0,0 +1,4 @@
+//WiFi cridentials for my IoT devices
+
+#define WIFI_SSID "Mikael_IoT"
+#define WIFI_PASSWORD "xFTK9wboIjl9992p"
